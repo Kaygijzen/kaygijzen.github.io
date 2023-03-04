0b4a2551fe4ba902c8208924671289674a7d8837
Kay Gijzen <58943291+Kaygijzen@users.noreply.github.com>
2023-03-04 18:39:29 +0100

    empty homepage

--- src/components/Hiro.js
+++ src/components/Hiro.js
@@ -20,14 +20,14 @@ export default function Hiro () {
             null
         }
         <div id="home" className="flex w-full h-screen flex-col md:flex-row gap-5 items-center justify-center text-white relative">
-            {/* <div className='md:w-3/6 md:p-4'>
-                <img data-aos="flip-right" data-aos-duration="1500" data-aos-offset="200"  src={profile} alt="profile" onLoad={() => setLoaded(false)} />
-            </div> */}
+            <div className='md:w-3/6 md:p-4'>
+                {/* <img data-aos="flip-right" data-aos-duration="1500" data-aos-offset="200"  src={profile} alt="profile" onLoad={() => setLoaded(false)} /> */}
+            </div>
             <div className='md:w-3/6' data-aos="fade-right" data-aos-duration="1000" data-aos-offset="100" >
                 <div className="flex flex-col w-full mt-8">
                     <h1 className="text-xl text-gray-400">Hi, I'm</h1>
                     <h1 className="text-2xl font-bold">Kay Gijzen</h1>
-                    <p class="text-xl font-bold text-gray-300">Software Engineer</p>
+                    <p className="text-xl font-bold text-gray-300">Software Engineer</p>
                 </div>
 
                 <ul className='flex mt-2 gap-4 items-center'>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,12 +7,11 @@ export default function Navbar() {
         <div className="flex justify-between items-center text-white">
             <img src={logo} className="App-logo w-10 rounded-full" alt="logo" />
             <ul className="hidden md:flex">
-            {/* <li className="p-4"><a href="#home" className="hover:underline">About</a></li>
-            <li className="p-4"><a href="#skills" className="hover:underline">Skills</a></li>
-            <li className="p-4"><a href="#honors" className="hover:underline">Honor & Awards</a></li>
-            <li className="p-4"><a href="#certs" className="hover:underline">Certfications</a></li> */}
+            {/* <li className="p-4"><a href="#home" className="hover:underline">About</a></li> */}
+            {/* <li className="p-4"><a href="#skills" className="hover:underline">Skills</a></li>
+            <li className="p-4"><a href="#honors" className="hover:underline">Honor & Awards</a></li> */}
             </ul>
-            {/* <a href={resume} rel="noreferrer" target="_blank" className=" bg-teal-500 rounded-full px-4 py-1 border border-teal-800 border border-4">Resume</a> */}
+            {/* <a rel="noreferrer" target="_blank" className=" bg-teal-500 rounded-full px-4 py-1 border border-teal-800 border border-4">Something</a> */}
         </div>
     </div>
     )
