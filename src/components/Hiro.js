5a85271b264fbd7fd81d529ed9712b679e2b4ab4
Kay Gijzen <58943291+Kaygijzen@users.noreply.github.com>
2023-03-05 01:05:52 +0100

    hiro profile image

--- src/components/Hiro.js
+++ src/components/Hiro.js
@@ -21,7 +21,7 @@ export default function Hiro () {
         }
         <div id="home" className="flex w-full h-screen flex-col md:flex-row gap-5 items-center justify-center text-white relative">
             <div className='md:w-3/6 md:p-4'>
-                {/* <img data-aos="flip-right" data-aos-duration="1500" data-aos-offset="200"  src={profile} alt="profile" onLoad={() => setLoaded(false)} /> */}
+                <img data-aos="flip-right" data-aos-duration="1500" data-aos-offset="200"  src={profile} alt="profile" onLoad={() => setLoaded(false)} />
             </div>
             <div className='md:w-3/6' data-aos="fade-right" data-aos-duration="1000" data-aos-offset="100" >
                 <div className="flex flex-col w-full mt-8">
