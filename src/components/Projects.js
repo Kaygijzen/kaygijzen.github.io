ccb3d07a4c9aa3c0bf3934a91a79fa84263365ca
Kay Gijzen <58943291+Kaygijzen@users.noreply.github.com>
2023-05-21 13:23:45 +0200

    punctuation

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,7 +9,7 @@ export default function Projects(){
             <p className="font-light text-gray-400">Here are some of my public repositories</p>
 
             <div className="flex flex-col md:flex-row mt-4 gap-5">
-                <HonorCard name="Discrete Markov Decision Process" link="https://github.com/Kaygijzen/MDP-Dynamic-Programming" desc="Python implementation of dynamic programming algorithms to solve a Markov decision process" />
+                <HonorCard name="Discrete Markov Decision Process" link="https://github.com/Kaygijzen/MDP-Dynamic-Programming" desc="Python implementation of dynamic programming algorithms to solve a Markov decision process." />
                 <HonorCard name="Sudoku" link="https://github.com/Kaygijzen/sudoku" desc="Pure Java implementation of a sudoku solver using a brute force algorithm." />
                 <HonorCard name="Graphs & Trees" link="https://github.com/Kaygijzen/graphsAndTrees" desc="Python implementation of a various operations (such as traversal, insertion, deletion and search) on graphs and trees." />
                 <HonorCard name="Tic-Tac-Toe" link="https://github.com/Kaygijzen/tictactoe" desc="C++ implementation of a decision-making algorithm (minimax) for tic-tac-toe." />
